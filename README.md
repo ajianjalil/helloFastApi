2c8f5aa5e54983e83a43f46d6125f1936824948c
Ajithkumar <ajith.kumar@calpinetech.com>
2023-05-02 15:49:50 +0530

    added files related to fast api

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# helloFastApi
+# inorder to start the application
+uvicorn main:app --reload
+
+
+please refer https://youtu.be/UIv6UyOIUAk
+https://youtu.be/f3CyAmelnEY?list=PLqAmigZvYxIL9dnYeZEhMoHcoP4zop8-p
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("HelloWorld")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"Message": "Ajith is a fast learner"}
+
+
+@app.post("/")
+async def post():
+    return {"Message": " Message is from post method"}
